a900d1727476b98dcf68e0e6173424678d79abcb
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-02-27 20:38:03 +0100

    movement fixed

--- Ship.gd
+++ Ship.gd
@@ -1,5 +1,8 @@
 extends Area2D
 export(int) var SPEED = 100
+
 func _process(delta):
-	if Imput.is_action_pressed("ui_up")
+	if Input.is_action_pressed("ui_up"):
 		position.y -= SPEED * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += SPEED * delta

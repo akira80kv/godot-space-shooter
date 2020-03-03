ec6caa76897fabbc4e59a9bca0c686925b4401cf
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-03-03 19:47:08 +0100

    PauseMenu added

---
+++ Node.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://World.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://StartMenu.tscn")
--- World.gd
+++ World.gd
@@ -2,8 +2,14 @@ extends Node
 
 var score = 0 setget set_score
 
+
 onready var scoreLabel = $ScoreLabel
 
 func set_score(value):
 	score = value
 	scoreLabel.text = "Score = " + str(score)
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene("res://PauseMenu.tscn")
+

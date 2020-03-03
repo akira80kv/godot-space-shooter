b4e62c610e3fd5a1f49fdb1703f7089cafe4882b
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-03-03 20:09:58 +0100

    StartMenu made better and removed pause screen

--- StartMenu.gd
+++ StartMenu.gd
@@ -5,3 +5,5 @@ extends Node
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene("res://World.tscn")
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit
--- World.gd
+++ World.gd
@@ -9,7 +9,7 @@ func set_score(value):
 	score = value
 	scoreLabel.text = "Score = " + str(score)
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene("res://PauseMenu.tscn")
+#func _process(delta):
+#	if Input.is_action_just_pressed("ui_cancel"):
+#		get_tree().change_scene("res://PauseMenu.tscn")
 

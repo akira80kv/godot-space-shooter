8d535adc52fff1dd571ea7309d2bd9a86d10e81b
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-03-04 20:39:42 +0100

    SaveAndLoad script in progress

--- SaveAndLoad.gd
+++ SaveAndLoad.gd
@@ -1,7 +1,19 @@
 extends Node
 
+const SAVE_DATA_PATH = "res://save_data.json"
 var default_save_data = {
-	highscore = 0,
+	highscore = 0
 }
 
 func _ready():
+	var a = "highscore"
+	print(str(default_save_data[a]))
+	
+func save_data_to_file(save_data):
+	var json_string = to_json(save_data)
+	var save_file = File.new()
+	save_file.open(SAVE_DATA_PATH, File.WRITE)
+	save_file.store_line(json_string)
+	save_file.close()
+	
+

d57456e7c6ed35dc3d336d1adb5a3fa03c6eda0a
akira80kv <61351191+akira80kv@users.noreply.github.com>
2020-03-05 20:07:55 +0100

    SaveAndLoad script still in progress

--- World.gd
+++ World.gd
@@ -7,8 +7,7 @@ onready var scoreLabel = $ScoreLabel
 
 func set_score(value):
 	score = value
-	scoreLabel.text = "Score = " + str(score)
-
+	update_score_label()
 #func _process(delta):
 #	if Input.is_action_just_pressed("ui_cancel"):
 #		get_tree().change_scene("res://PauseMenu.tscn")
@@ -16,5 +15,20 @@ func set_score(value):
 
 
 func _on_Ship_player_death():
+	update_save_data()
 	yield(get_tree().create_timer(1), "timeout")
 	get_tree().change_scene("res://GameOverScreen.tscn")
+	
+	
+
+func update_score_label():
+	scoreLabel.text = "Score = " + str(score)
+	
+	
+
+func update_save_data():
+	var save_data = SaveAndLoad.load_data_from_file()
+	if score > save_data.highscore:
+		save_data.highscore = score
+		SaveAndLoad.save_data_to_file(save_data)
+	
